b2b7e2353c480b1698eec2892189d90fe422225c
just-do-it <125887496+thanhnd29@users.noreply.github.com>
2024-07-24 10:50:36 +0700

    fix bug 26
    ok

--- src/core/api/student-supervisor.api.ts
+++ src/core/api/student-supervisor.api.ts
@@ -63,3 +63,335 @@ export const studentSupervisorApi = {
         return list;
     },
 };
+
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { StudentSupervisor } from '../models/student-supervisor';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+
+export interface ICreateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+export interface IUpdateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+const baseUrl = '/student-supervisors';
+
+export const studentSupervisorApi = {
+    create: async (dto: ICreateStudentSupervisorDto) => {
+        const { data } = await http.post<StudentSupervisor>(`${baseUrl}/create-account`, dto);
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateStudentSupervisorDto) => {
+        const { data } = await http.put<StudentSupervisor>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<StudentSupervisor>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const studentSupervisors = await studentSupervisorApi.getAll();
+
+        const list: EnumListItem[] = studentSupervisors.map((item) => ({
+            id: item.studentSupervisorId,
+            label: item.supervisorName,
+            color: getColorWithId(item.studentSupervisorId),
+            slug: item.supervisorName,
+            name: item.supervisorName,
+            value: item.studentSupervisorId,
+        }));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+};
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { StudentSupervisor } from '../models/student-supervisor';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+
+export interface ICreateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+export interface IUpdateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+const baseUrl = '/student-supervisors';
+
+export const studentSupervisorApi = {
+    create: async (dto: ICreateStudentSupervisorDto) => {
+        const { data } = await http.post<StudentSupervisor>(`${baseUrl}/create-account`, dto);
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateStudentSupervisorDto) => {
+        const { data } = await http.put<StudentSupervisor>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<StudentSupervisor>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const studentSupervisors = await studentSupervisorApi.getAll();
+
+        const list: EnumListItem[] = studentSupervisors.map((item) => ({
+            id: item.studentSupervisorId,
+            label: item.supervisorName,
+            color: getColorWithId(item.studentSupervisorId),
+            slug: item.supervisorName,
+            name: item.supervisorName,
+            value: item.studentSupervisorId,
+        }));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+};
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { StudentSupervisor } from '../models/student-supervisor';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+
+export interface ICreateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+export interface IUpdateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+const baseUrl = '/student-supervisors';
+
+export const studentSupervisorApi = {
+    create: async (dto: ICreateStudentSupervisorDto) => {
+        const { data } = await http.post<StudentSupervisor>(`${baseUrl}/create-account`, dto);
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateStudentSupervisorDto) => {
+        const { data } = await http.put<StudentSupervisor>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<StudentSupervisor>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const studentSupervisors = await studentSupervisorApi.getAll();
+
+        const list: EnumListItem[] = studentSupervisors.map((item) => ({
+            id: item.studentSupervisorId,
+            label: item.supervisorName,
+            color: getColorWithId(item.studentSupervisorId),
+            slug: item.supervisorName,
+            name: item.supervisorName,
+            value: item.studentSupervisorId,
+        }));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+};
+
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { StudentSupervisor } from '../models/student-supervisor';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+
+export interface ICreateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+export interface IUpdateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+const baseUrl = '/student-supervisors';
+
+export const studentSupervisorApi = {
+    create: async (dto: ICreateStudentSupervisorDto) => {
+        const { data } = await http.post<StudentSupervisor>(`${baseUrl}/create-account`, dto);
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateStudentSupervisorDto) => {
+        const { data } = await http.put<StudentSupervisor>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<StudentSupervisor>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const studentSupervisors = await studentSupervisorApi.getAll();
+
+        const list: EnumListItem[] = studentSupervisors.map((item) => ({
+            id: item.studentSupervisorId,
+            label: item.supervisorName,
+            color: getColorWithId(item.studentSupervisorId),
+            slug: item.supervisorName,
+            name: item.supervisorName,
+            value: item.studentSupervisorId,
+        }));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+};
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { StudentSupervisor } from '../models/student-supervisor';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+
+export interface ICreateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+export interface IUpdateStudentSupervisorDto
+    extends Pick<StudentSupervisor, 'address' | 'code' | 'description' | 'supervisorName' | 'phone' | 'password' | 'studentInClassId' | 'schoolId'> { }
+
+const baseUrl = '/student-supervisors';
+
+export const studentSupervisorApi = {
+    create: async (dto: ICreateStudentSupervisorDto) => {
+        const { data } = await http.post<StudentSupervisor>(`${baseUrl}/create-account`, dto);
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateStudentSupervisorDto) => {
+        const { data } = await http.put<StudentSupervisor>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<StudentSupervisor>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<StudentSupervisor>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const studentSupervisors = await studentSupervisorApi.getAll();
+
+        const list: EnumListItem[] = studentSupervisors.map((item) => ({
+            id: item.studentSupervisorId,
+            label: item.supervisorName,
+            color: getColorWithId(item.studentSupervisorId),
+            slug: item.supervisorName,
+            name: item.supervisorName,
+            value: item.studentSupervisorId,
+        }));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+};
--- src/core/api/violation-group.api.ts
+++ src/core/api/violation-group.api.ts
@@ -457,3 +457,465 @@ export const violationGroupApi = {
         return list;
     },
 };
+
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { ViolationGroup, ViolationGroupStatus } from '../models/violation-group';
+import { getColorWithId } from '../utils/api.helper';
+import { Colors } from '../utils/colors.helper';
+import http from './http';
+
+export interface ICreateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status'> { }
+
+export interface IUpdateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status' | 'schoolId'> { }
+
+const baseUrl = '/violation-groups';
+
+export const violationGroupApi = {
+    create: async (dto: ICreateViolationGroupDto) => {
+        const { data } = await http.post<ViolationGroup>(`${baseUrl}`, dto);
+
+        console.log(data);
+        
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateViolationGroupDto) => {
+        const { data } = await http.put<ViolationGroup>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<ViolationGroup>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search: string) => {
+        const violationGroups = await violationGroupApi.getAll();
+
+        const list: EnumListItem[] = violationGroups.map((item) => ({
+            id: item.violationGroupId,
+            label: item.vioGroupName,
+            color: getColorWithId(item.violationGroupId),
+            slug: item.vioGroupName,
+            name: item.vioGroupName,
+            value: item.violationGroupId,
+        }));
+
+        if (search) {
+            {
+                return list.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+            }
+        }
+
+        return list;
+    },
+    getEnumStatuses: async (search?: string) => {
+        const list: EnumListItem[] = [
+            {
+                color: Colors.GREEN,
+                id: ViolationGroupStatus.ACTIVE,
+                label: 'Active',
+                name: 'Active',
+                slug: ViolationGroupStatus.ACTIVE,
+                value: ViolationGroupStatus.ACTIVE,
+            },
+            {
+                color: Colors.RED,
+                id: ViolationGroupStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: ViolationGroupStatus.INACTIVE,
+                value: ViolationGroupStatus.INACTIVE,
+            },
+        ];
+
+        return list;
+    },
+};
+
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { ViolationGroup, ViolationGroupStatus } from '../models/violation-group';
+import { getColorWithId } from '../utils/api.helper';
+import { Colors } from '../utils/colors.helper';
+import http from './http';
+
+export interface ICreateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status'> { }
+
+export interface IUpdateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status' | 'schoolId'> { }
+
+const baseUrl = '/violation-groups';
+
+export const violationGroupApi = {
+    create: async (dto: ICreateViolationGroupDto) => {
+        const { data } = await http.post<ViolationGroup>(`${baseUrl}`, dto);
+
+        console.log(data);
+        
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateViolationGroupDto) => {
+        const { data } = await http.put<ViolationGroup>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<ViolationGroup>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search: string) => {
+        const violationGroups = await violationGroupApi.getAll();
+
+        const list: EnumListItem[] = violationGroups.map((item) => ({
+            id: item.violationGroupId,
+            label: item.vioGroupName,
+            color: getColorWithId(item.violationGroupId),
+            slug: item.vioGroupName,
+            name: item.vioGroupName,
+            value: item.violationGroupId,
+        }));
+
+        if (search) {
+            {
+                return list.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+            }
+        }
+
+        return list;
+    },
+    getEnumStatuses: async (search?: string) => {
+        const list: EnumListItem[] = [
+            {
+                color: Colors.GREEN,
+                id: ViolationGroupStatus.ACTIVE,
+                label: 'Active',
+                name: 'Active',
+                slug: ViolationGroupStatus.ACTIVE,
+                value: ViolationGroupStatus.ACTIVE,
+            },
+            {
+                color: Colors.RED,
+                id: ViolationGroupStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: ViolationGroupStatus.INACTIVE,
+                value: ViolationGroupStatus.INACTIVE,
+            },
+        ];
+
+        return list;
+    },
+};
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { ViolationGroup, ViolationGroupStatus } from '../models/violation-group';
+import { getColorWithId } from '../utils/api.helper';
+import { Colors } from '../utils/colors.helper';
+import http from './http';
+
+export interface ICreateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status'> { }
+
+export interface IUpdateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status' | 'schoolId'> { }
+
+const baseUrl = '/violation-groups';
+
+export const violationGroupApi = {
+    create: async (dto: ICreateViolationGroupDto) => {
+        const { data } = await http.post<ViolationGroup>(`${baseUrl}`, dto);
+
+        console.log(data);
+        
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateViolationGroupDto) => {
+        const { data } = await http.put<ViolationGroup>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<ViolationGroup>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search: string) => {
+        const violationGroups = await violationGroupApi.getAll();
+
+        const list: EnumListItem[] = violationGroups.map((item) => ({
+            id: item.violationGroupId,
+            label: item.vioGroupName,
+            color: getColorWithId(item.violationGroupId),
+            slug: item.vioGroupName,
+            name: item.vioGroupName,
+            value: item.violationGroupId,
+        }));
+
+        if (search) {
+            {
+                return list.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+            }
+        }
+
+        return list;
+    },
+    getEnumStatuses: async (search?: string) => {
+        const list: EnumListItem[] = [
+            {
+                color: Colors.GREEN,
+                id: ViolationGroupStatus.ACTIVE,
+                label: 'Active',
+                name: 'Active',
+                slug: ViolationGroupStatus.ACTIVE,
+                value: ViolationGroupStatus.ACTIVE,
+            },
+            {
+                color: Colors.RED,
+                id: ViolationGroupStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: ViolationGroupStatus.INACTIVE,
+                value: ViolationGroupStatus.INACTIVE,
+            },
+        ];
+
+        return list;
+    },
+};
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { ViolationGroup, ViolationGroupStatus } from '../models/violation-group';
+import { getColorWithId } from '../utils/api.helper';
+import { Colors } from '../utils/colors.helper';
+import http from './http';
+
+export interface ICreateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status'> { }
+
+export interface IUpdateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status' | 'schoolId'> { }
+
+const baseUrl = '/violation-groups';
+
+export const violationGroupApi = {
+    create: async (dto: ICreateViolationGroupDto) => {
+        const { data } = await http.post<ViolationGroup>(`${baseUrl}`, dto);
+
+        console.log(data);
+        
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateViolationGroupDto) => {
+        const { data } = await http.put<ViolationGroup>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<ViolationGroup>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search: string) => {
+        const violationGroups = await violationGroupApi.getAll();
+
+        const list: EnumListItem[] = violationGroups.map((item) => ({
+            id: item.violationGroupId,
+            label: item.vioGroupName,
+            color: getColorWithId(item.violationGroupId),
+            slug: item.vioGroupName,
+            name: item.vioGroupName,
+            value: item.violationGroupId,
+        }));
+
+        if (search) {
+            {
+                return list.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+            }
+        }
+
+        return list;
+    },
+    getEnumStatuses: async (search?: string) => {
+        const list: EnumListItem[] = [
+            {
+                color: Colors.GREEN,
+                id: ViolationGroupStatus.ACTIVE,
+                label: 'Active',
+                name: 'Active',
+                slug: ViolationGroupStatus.ACTIVE,
+                value: ViolationGroupStatus.ACTIVE,
+            },
+            {
+                color: Colors.RED,
+                id: ViolationGroupStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: ViolationGroupStatus.INACTIVE,
+                value: ViolationGroupStatus.INACTIVE,
+            },
+        ];
+
+        return list;
+    },
+};
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { ViolationGroup, ViolationGroupStatus } from '../models/violation-group';
+import { getColorWithId } from '../utils/api.helper';
+import { Colors } from '../utils/colors.helper';
+import http from './http';
+
+export interface ICreateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status'> { }
+
+export interface IUpdateViolationGroupDto extends Omit<ViolationGroup, 'violationGroupId' | 'schoolName' | 'status' | 'schoolId'> { }
+
+const baseUrl = '/violation-groups';
+
+export const violationGroupApi = {
+    create: async (dto: ICreateViolationGroupDto) => {
+        const { data } = await http.post<ViolationGroup>(`${baseUrl}`, dto);
+
+        console.log(data);
+        
+
+        return data;
+    },
+    update: async (id: number, dto: IUpdateViolationGroupDto) => {
+        const { data } = await http.put<ViolationGroup>(`${baseUrl}/${id}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<ViolationGroup>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<ViolationGroup>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search: string) => {
+        const violationGroups = await violationGroupApi.getAll();
+
+        const list: EnumListItem[] = violationGroups.map((item) => ({
+            id: item.violationGroupId,
+            label: item.vioGroupName,
+            color: getColorWithId(item.violationGroupId),
+            slug: item.vioGroupName,
+            name: item.vioGroupName,
+            value: item.violationGroupId,
+        }));
+
+        if (search) {
+            {
+                return list.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
+            }
+        }
+
+        return list;
+    },
+    getEnumStatuses: async (search?: string) => {
+        const list: EnumListItem[] = [
+            {
+                color: Colors.GREEN,
+                id: ViolationGroupStatus.ACTIVE,
+                label: 'Active',
+                name: 'Active',
+                slug: ViolationGroupStatus.ACTIVE,
+                value: ViolationGroupStatus.ACTIVE,
+            },
+            {
+                color: Colors.RED,
+                id: ViolationGroupStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: ViolationGroupStatus.INACTIVE,
+                value: ViolationGroupStatus.INACTIVE,
+            },
+        ];
+
+        return list;
+    },
+};
+
