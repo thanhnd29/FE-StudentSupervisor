f8959a4292ba23e07f171c72f86de8d2f35a4844
just-do-it <125887496+thanhnd29@users.noreply.github.com>
2024-07-27 20:14:26 +0700

    fix bug 39
    test ok

--- src/core/api/time.api.ts
+++ src/core/api/time.api.ts
@@ -23,3 +23,30 @@ export const timeApi = {
         await http.delete(`${baseUrl}/${id}`);
     },
 };
+
+import { BaseResponse, ResponseList } from '../models/common';
+import { Time } from '../models/time';
+import http from './http';
+
+const baseUrl = '/times';
+
+export const timeApi = {
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<Time>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data ? data.data : [];
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<Time>>(`${baseUrl}/${id}`);
+
+        return data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+};
+
