9919de628d7a5e1c77c64c111c64d36517aeba4b
just-do-it <125887496+thanhnd29@users.noreply.github.com>
2024-07-30 20:26:55 +0700

    Fix bug 50
    ok

--- src/core/api/class.api.ts
+++ src/core/api/class.api.ts
@@ -102,3 +102,109 @@ export const classApi = {
             return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
         }
 };
+
+
+import { Class } from '../models/class';
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+
+export interface ICreateClassDto extends Omit<Class, 'classId'> { }
+
+export interface IUpdateClassDto extends Class { }
+
+const baseUrl = '/classes';
+
+export const classApi = {
+    create: async (dto: ICreateClassDto) => {
+        const { data } = await http.post<Class>(`${baseUrl}`, dto);
+
+        return data;
+    },
+    update: async (dto: IUpdateClassDto) => {
+        const { data } = await http.put<Class>(`${baseUrl}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<Class>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<Class>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<Class>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        await http.delete(`${baseUrl}/${id}`);
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const classes = await classApi.getAll();
+
+        const list: EnumListItem[] = classes
+            .map((item) => ({
+                id: item.classId,
+                label: item.name,
+                color: getColorWithId(item.classId),
+                slug: item.name,
+                name: item.name,
+                value: item.classId,
+            }))
+            .sort((a, b) => a.label.localeCompare(b.label));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+
+    getEnumSelectOptions: async (search?: string) => {
+        const classes = await classApi.getAll();
+
+        const list: EnumListItem[] = classes
+            .map((item) => ({
+                id: item.classId,
+                label: item.name,
+                color: getColorWithId(item.classId),
+                slug: item.name,
+                name: item.name,
+                value: item.classId,
+            }))
+            .sort((a, b) => a.label.localeCompare(b.label));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+
+    getEnumSelectOptions: async (search?: string) => {
+        const classes = await classApi.getAll();
+
+        const list: EnumListItem[] = classes
+            .map((item) => ({
+                id: item.classId,
+                label: item.name,
+                color: getColorWithId(item.classId),
+                slug: item.name,
+                name: item.name,
+                value: item.classId,
+            }))
+            .sort((a, b) => a.label.localeCompare(b.label));
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+};
