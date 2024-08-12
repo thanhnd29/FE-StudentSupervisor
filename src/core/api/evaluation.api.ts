4b6b7263e047da1c34de972dd4099d48ef5348ee
just-do-it <125887496+thanhnd29@users.noreply.github.com>
2024-08-12 19:21:19 +0700

    fix 101 update evaluation
    tesst ok

--- src/core/api/evaluation.api.ts
+++ src/core/api/evaluation.api.ts
@@ -86,6 +86,15 @@ export const evaluationApi = {
                 slug: EvaluationStatus.INACTIVE,
                 value: EvaluationStatus.INACTIVE,
             },
+
+            {
+                color: Colors.RED,
+                id: EvaluationStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: EvaluationStatus.INACTIVE,
+                value: EvaluationStatus.INACTIVE,
+            },
         ];
 
         return list;
