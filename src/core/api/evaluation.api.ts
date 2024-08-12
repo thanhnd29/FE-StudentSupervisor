286daa84b4854c9af091887b07958a0fbff3c2a7
just-do-it <125887496+thanhnd29@users.noreply.github.com>
2024-08-12 19:42:09 +0700

    fix bug 103  chọn Class của Evaluation
    ok

--- src/core/api/evaluation.api.ts
+++ src/core/api/evaluation.api.ts
@@ -110,3 +110,116 @@ export const evaluationApi = {
         return list;
     },
 };
+
+import moment from 'moment';
+
+import { BaseResponse, EnumListItem, ResponseList } from '../models/common';
+import { Evaluation, EvaluationStatus } from '../models/evalution';
+import { getColorWithId } from '../utils/api.helper';
+import http from './http';
+import { Colors } from '../utils/colors.helper';
+
+export interface ICreateEvaluationDto extends Omit<Evaluation, 'evaluationId' | 'className' | 'status' | 'points'> { }
+
+export interface IUpdateEvaluationDto extends Pick<Evaluation, 'evaluationId' | 'classId' | 'description' | 'from' | 'to' | 'points'> { }
+
+const baseUrl = '/evaluations';
+
+export const evaluationApi = {
+    create: async (dto: ICreateEvaluationDto) => {
+        const { data } = await http.post<Evaluation>(`${baseUrl}`, dto);
+
+        return data;
+    },
+    update: async (dto: IUpdateEvaluationDto) => {
+        const { data } = await http.put<Evaluation>(`${baseUrl}`, dto);
+
+        return data;
+    },
+    getAll: async () => {
+        const { data } = await http.get<ResponseList<Evaluation>>(`${baseUrl}`, {
+            params: {
+                sortOrder: 'desc',
+            },
+        });
+
+        return data.data;
+    },
+    getBySchool: async (id: number) => {
+        const { data } = await http.get<ResponseList<Evaluation>>(`${baseUrl}/school/${id}`);
+
+        return data.data;
+    },
+    getById: async (id: number) => {
+        const { data } = await http.get<BaseResponse<Evaluation>>(`${baseUrl}/${id}`);
+
+        return data.data;
+    },
+    delete: async (id: number) => {
+        const { data } = await http.delete(`${baseUrl}/${id}`);
+
+        return data;
+    },
+    getEnumSelectOptions: async (search?: string) => {
+        const classes = await evaluationApi.getAll();
+
+        const list: EnumListItem[] = classes.map((item) => {
+            const label = `${moment(item.from).format('DD/MM/YYYY')} - ${moment(item.to).format('DD/MM/YYYY')}`;
+            return {
+                id: item.evaluationId,
+                label,
+                color: getColorWithId(item.evaluationId),
+                slug: label,
+                name: label,
+                value: item.evaluationId,
+            };
+            const list: EnumListItem[] = classes.map((item) => {
+                const label = `${moment(item.from).format('DD/MM/YYYY')} - ${moment(item.to).format('DD/MM/YYYY')}`;
+                return {
+                    id: item.evaluationId,
+                    label,
+                    color: getColorWithId(item.evaluationId),
+                    slug: label,
+                    name: label,
+                    value: item.evaluationId,
+                };
+        });
+
+        if (search) {
+            return list.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        return list;
+    },
+    getEnumStatuses: async (search?: string) => {
+        const list: EnumListItem[] = [
+            {
+                color: Colors.GREEN,
+                id: EvaluationStatus.ACTIVE,
+                label: 'Active',
+                name: 'Active',
+                slug: EvaluationStatus.ACTIVE,
+                value: EvaluationStatus.ACTIVE,
+            },
+            {
+                color: Colors.RED,
+                id: EvaluationStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: EvaluationStatus.INACTIVE,
+                value: EvaluationStatus.INACTIVE,
+            },
+
+            {
+                color: Colors.RED,
+                id: EvaluationStatus.INACTIVE,
+                label: 'Inactive',
+                name: 'Inactive',
+                slug: EvaluationStatus.INACTIVE,
+                value: EvaluationStatus.INACTIVE,
+            },
+        ];
+
+        return list;
+    },
+};
